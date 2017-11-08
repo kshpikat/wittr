d3692046f4939053ea0fb3a91a8894541d3776d9
Constantine Shpikat <cshpikat@sugarcrm.com>
2017-11-08 16:25:12 +0100

    Commit

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,19 +1,34 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('wittr-static-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-      ]);
-    })
-  );
+const cacheName = 'wittr-static-v7';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll([
+                '/',
+                'js/main.js',
+                'css/main.css',
+                'imgs/icon.png',
+                'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+                'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
+self.addEventListener('fetch', function (event) {
+    //event.respondWith(
+    //    caches.open(cacheName).then(function (cache){
+    //        cache.match(event.request).then(function(response) {
+    //            return response || fetch(event.request);
+    //        })
+    //    })
+    //);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            console.log(response);
+            if (response) return response;
+            return fetch(event.request);
+        })
+    );
 });
+
