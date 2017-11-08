80cc580cb1694fc7beee9bc12cce95a88c7c63b8
Constantine Shpikat <cshpikat@sugarcrm.com>
2017-11-08 14:24:44 +0100

    Commit

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,19 @@
-self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+self.addEventListener('fetch', function (event) {
+    //console.log(event.request);
+    let url = event.request.url;
+    console.log(url);
+    if (url.endsWith('.jpg')) {
+        event.respondWith(
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
+    //new Response('Hello <a href="" class="a-winner-is-me">link</a>', {
+    //    headers: {
+    //        'Content-Type': 'text/html; charset=UTF-8'
+    //    }
+    //})
+  //event.respondWith(
+  //  fetch('/imgs/dr-evil.gif')
+  //);
 });
+
