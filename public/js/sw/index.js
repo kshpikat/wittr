a772880b8f5309494121507ac0552fe2d5b5dc51
Constantine Shpikat <cshpikat@sugarcrm.com>
2017-11-08 14:10:02 +0100

    Commit changes

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,18 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
   console.log(event.request);
+    fetch('/foo').then(function (response){
+        return response.json();
+    }).then(function (data){
+       console.log(data)
+    }).catch(function (err){
+        console.error(err);
+    });
+    event.respondWith(
+        fetch('/imgs/dr-evil.gif')
+        //new Response('Hello <a href="" class="a-winner-is-me">link</a>', {
+        //    headers: {
+        //        'Content-Type':'text/html; charset=UTF-8'
+        //    }
+        //})
+    );
 });
