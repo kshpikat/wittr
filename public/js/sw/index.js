e8557775f9ba35a697653c42dc7ded748157159d
Constantine Shpikat <cshpikat@sugarcrm.com>
2017-11-08 14:55:11 +0100

    Commit

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,11 +1,12 @@
+self.addEventListener('install', function (event){
+    event.waitUntil()
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).then(function(response) {
       if (response.status === 404) {
-        // TODO: instead, respond with the gif at
-        // /imgs/dr-evil.gif
-        // using a network request
-        return new Response("Whoops, not found");
+        return fetch('/imgs/dr-evil.gif');
       }
       return response;
     }).catch(function() {
