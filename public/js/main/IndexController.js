fffbf8bf01fa3aa9eeb1326d290779766b1db7e5
Constantine Shpikat <cshpikat@sugarcrm.com>
2017-11-13 18:51:31 +0100

    Commit

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -3,139 +3,178 @@ import ToastsView from './views/Toasts';
 import idb from 'idb';
 
 function openDatabase() {
-  // If the browser doesn't support service worker,
-  // we don't care about having a database
-  if (!navigator.serviceWorker) {
-    return Promise.resolve();
-  }
+    // If the browser doesn't support service worker,
+    // we don't care about having a database
+    if (!navigator.serviceWorker) {
+        return Promise.resolve();
+    }
 
-  // TODO: return a promise for a database called 'wittr'
-  // that contains one objectStore: 'wittrs'
-  // that uses 'id' as its key
-  // and has an index called 'by-date', which is sorted
-  // by the 'time' property
+
+    return idb.open('wittr', 1, function (upgradeDb) {
+        switch (upgradeDb.oldVersion) {
+            case 0:
+                var store = upgradeDb.createObjectStore('wittrs', {keyPath: 'id'});
+                store.createIndex('by-date', 'time');
+
+
+            //var keyValStore = upgradeDb.createObjectStore('keyval');
+            //keyValStore.put("world", "hello");
+            //case 1:
+            //  upgradeDb.createObjectStore('people', { keyPath: 'name' });
+            //case 2:
+            //  var peopleStore = upgradeDb.transaction.objectStore('people');
+            //  peopleStore.createIndex('animal', 'favoriteAnimal');
+            //case 3:
+            //  peopleStore = upgradeDb.transaction.objectStore('people');
+            //  peopleStore.createIndex('age', 'age');
+        }
+    });
 }
 
 export default function IndexController(container) {
-  this._container = container;
-  this._postsView = new PostsView(this._container);
-  this._toastsView = new ToastsView(this._container);
-  this._lostConnectionToast = null;
-  this._openSocket();
-  this._dbPromise = openDatabase();
-  this._registerServiceWorker();
+    this._container = container;
+    this._postsView = new PostsView(this._container);
+    this._toastsView = new ToastsView(this._container);
+    this._lostConnectionToast = null;
+    this._openSocket();
+    this._dbPromise = openDatabase();
+    this._registerServiceWorker();
 }
 
-IndexController.prototype._registerServiceWorker = function() {
-  if (!navigator.serviceWorker) return;
+IndexController.prototype._registerServiceWorker = function () {
+    if (!navigator.serviceWorker) return;
 
-  var indexController = this;
+    var indexController = this;
 
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
-    if (!navigator.serviceWorker.controller) {
-      return;
-    }
+    navigator.serviceWorker.register('/sw.js').then(function (reg) {
+        if (!navigator.serviceWorker.controller) {
+            return;
+        }
 
-    if (reg.waiting) {
-      indexController._updateReady(reg.waiting);
-      return;
-    }
+        if (reg.waiting) {
+            indexController._updateReady(reg.waiting);
+            return;
+        }
 
-    if (reg.installing) {
-      indexController._trackInstalling(reg.installing);
-      return;
-    }
+        if (reg.installing) {
+            indexController._trackInstalling(reg.installing);
+            return;
+        }
 
-    reg.addEventListener('updatefound', function() {
-      indexController._trackInstalling(reg.installing);
+        reg.addEventListener('updatefound', function () {
+            indexController._trackInstalling(reg.installing);
+        });
     });
-  });
 
-  // Ensure refresh is only called once.
-  // This works around a bug in "force update on reload".
-  var refreshing;
-  navigator.serviceWorker.addEventListener('controllerchange', function() {
-    if (refreshing) return;
-    window.location.reload();
-    refreshing = true;
-  });
+    // Ensure refresh is only called once.
+    // This works around a bug in "force update on reload".
+    var refreshing;
+    navigator.serviceWorker.addEventListener('controllerchange', function () {
+        if (refreshing) return;
+        window.location.reload();
+        refreshing = true;
+    });
 };
 
-IndexController.prototype._trackInstalling = function(worker) {
-  var indexController = this;
-  worker.addEventListener('statechange', function() {
-    if (worker.state == 'installed') {
-      indexController._updateReady(worker);
-    }
-  });
+IndexController.prototype._trackInstalling = function (worker) {
+    var indexController = this;
+    worker.addEventListener('statechange', function () {
+        if (worker.state == 'installed') {
+            indexController._updateReady(worker);
+        }
+    });
 };
 
-IndexController.prototype._updateReady = function(worker) {
-  var toast = this._toastsView.show("New version available", {
-    buttons: ['refresh', 'dismiss']
-  });
+IndexController.prototype._updateReady = function (worker) {
+    var toast = this._toastsView.show("New version available", {
+        buttons: ['refresh', 'dismiss']
+    });
 
-  toast.answer.then(function(answer) {
-    if (answer != 'refresh') return;
-    worker.postMessage({action: 'skipWaiting'});
-  });
+    toast.answer.then(function (answer) {
+        if (answer != 'refresh') return;
+        worker.postMessage({action: 'skipWaiting'});
+    });
 };
 
 // open a connection to the server for live updates
-IndexController.prototype._openSocket = function() {
-  var indexController = this;
-  var latestPostDate = this._postsView.getLatestPostDate();
+IndexController.prototype._openSocket = function () {
+    var indexController = this;
+    var latestPostDate = this._postsView.getLatestPostDate();
 
-  // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
+    // create a url pointing to /updates with the ws protocol
+    var socketUrl = new URL('/updates', window.location);
+    socketUrl.protocol = 'ws';
 
-  if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
-  }
-
-  // this is a little hack for the settings page's tests,
-  // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
-
-  var ws = new WebSocket(socketUrl.href);
-
-  // add listeners
-  ws.addEventListener('open', function() {
-    if (indexController._lostConnectionToast) {
-      indexController._lostConnectionToast.hide();
+    if (latestPostDate) {
+        socketUrl.search = 'since=' + latestPostDate.valueOf();
     }
-  });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
-      indexController._onSocketMessage(event.data);
+    // this is a little hack for the settings page's tests,
+    // it isn't needed for Wittr
+    socketUrl.search += '&' + location.search.slice(1);
+
+    var ws = new WebSocket(socketUrl.href);
+
+    // add listeners
+    ws.addEventListener('open', function () {
+        if (indexController._lostConnectionToast) {
+            indexController._lostConnectionToast.hide();
+        }
     });
-  });
 
-  ws.addEventListener('close', function() {
-    // tell the user
-    if (!indexController._lostConnectionToast) {
-      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
-    }
+    ws.addEventListener('message', function (event) {
+        requestAnimationFrame(function () {
+            indexController._onSocketMessage(event.data);
+        });
+    });
 
-    // try and reconnect in 5 seconds
-    setTimeout(function() {
-      indexController._openSocket();
-    }, 5000);
-  });
+    ws.addEventListener('close', function () {
+        // tell the user
+        if (!indexController._lostConnectionToast) {
+            indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
+        }
+
+        // try and reconnect in 5 seconds
+        setTimeout(function () {
+            indexController._openSocket();
+        }, 5000);
+    });
 };
 
 // called when the web socket sends message data
-IndexController.prototype._onSocketMessage = function(data) {
-  var messages = JSON.parse(data);
+IndexController.prototype._onSocketMessage = function (data) {
+    var messages = JSON.parse(data);
 
-  this._dbPromise.then(function(db) {
-    if (!db) return;
+    this._dbPromise.then(function (db) {
+        if (!db) return;
+        var tx = db.transaction('wittrs', 'readwrite');
+        var store = tx.objectStore('wittrs');
+        messages.forEach(function (msg){
+            console.log('msg added', msg.id);
+            store.put(msg);
+        });
+        return tx.complete;
+    });
 
-    // TODO: put each message into the 'wittrs'
-    // object store.
-  });
-
-  this._postsView.addPosts(messages);
+    this._postsView.addPosts(messages);
 };
+
+//IndexController.prototype._onSocketMessage = function (data) {
+//    var messages = JSON.parse(data);
+//
+//    this._dbPromise.then(function (db) {
+//        if (!db) return;
+//        var tx = db.transaction('wittrs', 'readwrite');
+//        var store = tx.objectStore('wittrs');
+//        var store = tx.objectStore('wittrs');
+//        var store = tx.objectStore('wittrs');
+//        var store = tx.objectStore('wittrs');
+//        messages.forEach(function (msg){
+//            console.log('msg added', msg.id);
+//            store.put(msg);
+//        });
+//        return tx.complete;
+//    });
+//
+//    this._postsView.addPosts(messages);
+//};
